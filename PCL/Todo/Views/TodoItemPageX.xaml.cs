2f2117478e665c2d514dceba35dca490ec0709ec
Nguyen Hai Dang <haidang.cva@gmail.com>
2016-12-05 22:36:31 +0700

    Final project
    xamarin project

---
+++ PCL/Todo/Views/TodoItemPageX.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Todo
+{
+    public partial class TodoItemPageX : ContentPage
+    {
+        public TodoItemPageX()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, true);
+        }
+
+        void saveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            App.Database.SaveItem(todoItem);
+            this.Navigation.PopAsync();
+        }
+
+        void deleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            App.Database.DeleteItem(todoItem.ID);
+            this.Navigation.PopAsync();
+        }
+
+        void cancelClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            this.Navigation.PopAsync();
+        }
+
+    }
+}
